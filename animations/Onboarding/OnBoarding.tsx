1441ad54360d9038466149fbd2ed96124ac657c4
Kartikey Vaish <kartikeyvaish99@gmail.com>
2022-10-01 12:42:56 +0530

    :sparkles: created onboarding

---
+++ animations/Onboarding/OnBoarding.tsx
@@ -0,0 +1,92 @@
+import { StatusBar } from "expo-status-bar";
+import React, { useCallback } from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedRef,
+  useAnimatedScrollHandler,
+  useDerivedValue,
+  useSharedValue,
+} from "react-native-reanimated";
+import Page, { PAGE_WIDTH } from "./components/Page";
+import { BACKGROUND_COLOR, PAGES } from "./constants";
+import { AntDesign } from "@expo/vector-icons";
+import Dot from "./components/Dot";
+
+export default function OnBoarding() {
+  const translateX = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler({
+    onScroll: event => {
+      translateX.value = event.contentOffset.x;
+    },
+  });
+
+  const activeIndex = useDerivedValue(() => {
+    return Math.round(translateX.value / PAGE_WIDTH);
+  });
+
+  const scrollRef = useAnimatedRef<ScrollView>();
+
+  const onIconPress = useCallback(() => {
+    if (activeIndex.value === PAGES.length - 1) return;
+    scrollRef.current?.scrollTo({ x: PAGE_WIDTH * (activeIndex.value + 1) });
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <Animated.ScrollView
+        ref={scrollRef as any}
+        style={{ flex: 1 }}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onScroll={scrollHandler}
+        scrollEventThrottle={16}
+      >
+        {PAGES.map((page, index) => (
+          <Page key={index.toString()} page={page} translateX={translateX} index={index} />
+        ))}
+      </Animated.ScrollView>
+      <View style={styles.footer}>
+        {/* Paginator */}
+        <View style={[styles.fillCenter, { flexDirection: "row" }]}>
+          {PAGES.map((_, index) => {
+            return <Dot key={index.toString()} index={index} activeDotIndex={activeIndex} />;
+          })}
+        </View>
+        {/* Text Container */}
+        <View style={styles.fillCenter}>
+          <Text style={styles.text}>View Board</Text>
+        </View>
+        {/* iconContainer */}
+        <View style={styles.fillCenter}>
+          <AntDesign name="arrowright" size={24} color="black" onPress={onIconPress} />
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: BACKGROUND_COLOR,
+  },
+  footer: {
+    height: 50,
+    marginBottom: 50,
+    flexDirection: "row",
+  },
+  fillCenter: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 14,
+    textTransform: "uppercase",
+    letterSpacing: 1.7,
+    fontWeight: "500",
+  },
+});
