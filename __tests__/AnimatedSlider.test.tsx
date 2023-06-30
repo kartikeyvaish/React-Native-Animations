5473e7d74e757e342a8eedefc0320e5260b80388
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-01 01:13:34 +0530

    :sparkles: created test suite for AnimatedSlider

--- __tests__/AnimatedSlider.test.tsx
+++ __tests__/AnimatedSlider.test.tsx
@@ -1,13 +1,15 @@
 // Packages Imports (from node_modules)
+import { render } from "@testing-library/react-native";
 import renderer from "react-test-renderer";
 
 // Component Imports
 import AnimatedSlider from "../animations/AnimatedSlider/AnimatedSlider";
 
 describe("<AnimatedSlider />", () => {
-  it("should render correctly", () => {
-    const tree = renderer.create(<AnimatedSlider />);
-    expect(tree.toJSON()).toMatchSnapshot();
+  it("renders without errors", () => {
+    const { getByTestId } = render(<AnimatedSlider />);
+    const flatListComponent = getByTestId("animated-slider");
+    expect(flatListComponent).toBeTruthy();
   });
 
   it("should have 10 items", () => {
