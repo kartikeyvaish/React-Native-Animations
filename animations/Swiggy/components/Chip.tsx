3f113d4ca96765cf439e2ed295c7541f0e17efe3
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-07 01:12:58 +0530

    :sparkles: created chip component

---
+++ animations/Swiggy/components/Chip.tsx
@@ -0,0 +1,73 @@
+// Packages Imports (from node_modules)
+import { StyleSheet } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import Animated, { FadeIn, FadeOut, Layout } from "react-native-reanimated";
+
+// Local Imports (components/types/utils)
+import AnimatedText from "../../../components/AnimatedText";
+import { ChipsProps } from "../types/Types";
+import useThemeManager from "../../../hooks/useThemeManager";
+
+// functional component for Chips
+function Chips(props: ChipsProps) {
+  // Destructuring props
+  const { name, isSelected, onPress } = props;
+
+  const containerStyle = {
+    backgroundColor: isSelected ? "lightgrey" : undefined,
+  };
+
+  const { Theme } = useThemeManager();
+
+  // render
+  return (
+    <Animated.View layout={Layout}>
+      <RectButton style={styles.container} onPress={onPress}>
+        <Animated.View style={[styles.btnInnerContainer, containerStyle]} layout={Layout}>
+          {isSelected ? (
+            <AnimatedText
+              text='x'
+              style={styles.removeIcon}
+              entering={FadeIn.delay(200)}
+              exiting={FadeOut}
+              color={isSelected ? (Theme.dark ? "black" : undefined) : undefined}
+            />
+          ) : null}
+
+          <AnimatedText
+            text={name}
+            layout={Layout}
+            color={isSelected ? (Theme.dark ? "black" : undefined) : undefined}
+          />
+        </Animated.View>
+      </RectButton>
+    </Animated.View>
+  );
+}
+
+// exports
+export default Chips;
+
+// styles for Chips
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 10,
+    height: 30,
+    borderRadius: 100,
+  },
+  btnInnerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "lightgrey",
+    paddingLeft: 12,
+    paddingRight: 12,
+    borderRadius: 100,
+    flexDirection: "row",
+  },
+  removeIcon: {
+    fontSize: 15,
+    marginRight: 10,
+  },
+});
