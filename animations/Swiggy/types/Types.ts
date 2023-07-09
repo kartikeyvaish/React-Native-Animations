d7998f769f9f1d70394cb6f8539898f926df2135
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-10 00:36:13 +0530

    :sparkles: created types for component & mock data

--- animations/Swiggy/types/Types.ts
+++ animations/Swiggy/types/Types.ts
@@ -1,3 +1,7 @@
+
+import { ReactNode } from 'react';
+import { ViewStyle, StyleProp } from 'react-native';
+
 export interface FilterItemProps {
     id: string;
     name: string;
@@ -8,6 +12,8 @@ export interface ChipsProps {
     name: string;
     isSelected?: boolean;
     onPress?: () => void;
+    containerStyles?: StyleProp<ViewStyle>
+    children?: ReactNode
 }
 
 // interface for MenuScreenHeader component
@@ -18,3 +24,25 @@ export interface MenuScreenHeaderProps {
 
 // interface for RestaurantCard component
 export interface RestaurantCardProps { }
+
+export interface Restaurant {
+    id: string;
+    restaurantName: string;
+    location: string;
+    distance: string;
+    discountText: string;
+    rating: number;
+    ratingCountText: string;
+    deliveryDuration: string;
+    items: MenuItem[];
+}
+
+export interface MenuItem {
+    isVegetarian: boolean;
+    name: string;
+    price: number;
+    currency: string;
+    image: string;
+    description: string;
+    isCustomizable: boolean;
+}
