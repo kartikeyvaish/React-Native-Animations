508cd15b66c02b67628f71c20e13ca9eaf559392
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-10 12:56:28 +0530

    :zap: added more props to food item component

--- animations/Swiggy/components/FoodItem.tsx
+++ animations/Swiggy/components/FoodItem.tsx
@@ -1,19 +1,23 @@
 // Packages Imports (from node_modules)
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 // Local Imports (components/types/utils)
 import AppText from "../../../components/AppText";
-import FoodSymbol from "./FoodSymbol";
-import { MenuItem } from "../types/Types";
+import CardItemCounter from "./CardItemCounter";
 import Chips from "./Chip";
+import FoodSymbol from "./FoodSymbol";
 
-// interface for Fooditem component
-export interface FooditemProps extends MenuItem {}
+// Named imports
+import { MenuItem } from "../types/Types";
 
-// functional component for Fooditem
-function Fooditem(props: FooditemProps) {
+// interface for FoodItem component
+export interface FoodItemProps extends MenuItem {}
+
+// functional component for FoodItem
+function FoodItem(props: FoodItemProps) {
   // Destructuring props
-  const { isVegetarian, name, currency, price } = props;
+  const { isVegetarian, name, currency, price, image, isCustomizable } = props;
 
   // render
   return (
@@ -21,34 +25,58 @@ function Fooditem(props: FooditemProps) {
       <View style={styles.foodDetailsContainer}>
         <FoodSymbol color={isVegetarian ? "darkgreen" : "red"} />
 
-        <AppText text={name} size={18} style={{ fontWeight: "bold" }} marginTop={5} />
-        <AppText text={`${currency} ${price}`} size={16} marginTop={10} />
+        <AppText text={name} size={18} style={{ fontWeight: "bold" }} marginTop={5} color='black' />
+        <AppText text={`${currency} ${price}`} size={16} marginTop={10} color='black' />
 
-        <Chips
-          name='More Details'
-          containerStyles={{ marginLeft: 0, marginTop: 10, maxWidth: 120 }}
-        />
+        <Chips name='More Details' containerStyles={styles.moreDetailsChipContainer} color='black'>
+          <AntDesign name='right' size={10} color='black' style={{ marginTop: 2, marginLeft: 5 }} />
+        </Chips>
       </View>
 
-      <View style={styles.foodImageContainer}></View>
+      <View style={styles.foodImageContainer}>
+        <Image source={{ uri: image }} style={styles.foodItemImage} />
+        <CardItemCounter containerStyles={{ position: "absolute", bottom: -10 }} />
+        {isCustomizable ? <AppText text='Customizable' style={styles.customizableText} /> : null}
+      </View>
     </View>
   );
 }
 
 // exports
-export default Fooditem;
+export default FoodItem;
 
-// styles for Fooditem
+// styles for FoodItem
 const styles = StyleSheet.create({
   container: {
     width: "100%",
     padding: 10,
     flexDirection: "row",
+    paddingBottom: 60,
   },
   foodDetailsContainer: {
     flex: 1,
+    paddingTop: 20,
+    paddingBottom: 20,
   },
   foodImageContainer: {
     flex: 1,
+    alignItems: "center",
+  },
+  foodItemImage: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
+  customizableText: {
+    position: "absolute",
+    bottom: -35,
+    color: "grey",
+    fontSize: 12,
+  },
+  moreDetailsChipContainer: {
+    marginLeft: 0,
+    marginTop: 10,
+    maxWidth: 120,
   },
 });
