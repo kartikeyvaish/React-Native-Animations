76fca4e097ef1395b21c27465f23d53520ccae82
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-11 01:48:00 +0530

    :zap: added staggering animation to home screen

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,11 +1,14 @@
 // Packages Imports
 import { FlatList, StyleSheet } from "react-native";
+import Animated, { SlideInRight } from "react-native-reanimated";
 
 // Local Imports
 import AppContainer from "../components/AppContainer";
-import { AppScreenProps } from "../navigation/NavigationProps";
 import components from "../config/components";
 import MenuCard from "../components/MenuCard";
+
+// Named Imports
+import { AppScreenProps } from "../navigation/NavigationProps";
 
 // function component for HomeScreen
 function HomeScreen({ navigation }: AppScreenProps<"HomeScreen">) {
@@ -16,12 +19,14 @@ function HomeScreen({ navigation }: AppScreenProps<"HomeScreen">) {
         data={components}
         keyExtractor={item => item.id}
         showsVerticalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <MenuCard
-            title={item.name}
-            onPress={() => navigation.navigate(item.screen)}
-            key={item.id}
-          />
+        renderItem={({ item, index }) => (
+          <Animated.View entering={SlideInRight.delay(index * 20).springify()}>
+            <MenuCard
+              title={item.name}
+              onPress={() => navigation.navigate(item.screen)}
+              key={item.id}
+            />
+          </Animated.View>
         )}
       />
     </AppContainer>
