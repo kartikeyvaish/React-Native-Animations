9525198b6cf5b098e612f56cbb1392d02d5ebb66
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-11 00:44:01 +0530

    :zap: added testIDs to components

--- animations/Swiggy/components/CardItemCounter.tsx
+++ animations/Swiggy/components/CardItemCounter.tsx
@@ -1,5 +1,5 @@
 // Packages Imports (from node_modules)
-import { StyleSheet, ViewStyle, StyleProp, View, Text, Pressable } from "react-native";
+import { StyleSheet, ViewStyle, StyleProp, Text, Pressable } from "react-native";
 import Animated, { FadeIn, FadeOut, ZoomIn, ZoomOut } from "react-native-reanimated";
 import { FontAwesome } from "@expo/vector-icons";
 
@@ -24,6 +24,7 @@ function CardItemCounter(props: CardItemCounterProps) {
     <Pressable
       style={[styles.container, containerStyles]}
       onPress={currentValue < 1 ? onIncrement : undefined}
+      testID='card-item-test-id'
     >
       {currentValue < 1 ? (
         <AnimatedText
@@ -32,21 +33,24 @@ function CardItemCounter(props: CardItemCounterProps) {
           text='ADD'
           color='darkgreen'
           style={{ fontWeight: "bold" }}
+          testID='add-text-test-id'
         />
       ) : (
         <>
           <Animated.View entering={ZoomIn} exiting={ZoomOut} style={{ flex: 1.5 }}>
-            <Pressable style={styles.pressable} onPress={onDecrement}>
+            <Pressable style={styles.pressable} onPress={onDecrement} testID='minus-button-test-id'>
               <FontAwesome name='minus' size={12} color='darkgreen' />
             </Pressable>
           </Animated.View>
 
           <Animated.View entering={FadeIn.delay(50)} style={[styles.pressable, { flex: 2 }]}>
-            <Text style={{ color: "darkgreen", fontWeight: "bold" }}>{currentValue}</Text>
+            <Text style={{ color: "darkgreen", fontWeight: "bold" }} testID='value-test-id'>
+              {currentValue}
+            </Text>
           </Animated.View>
 
           <Animated.View entering={ZoomIn} exiting={ZoomOut} style={{ flex: 1.5 }}>
-            <Pressable style={styles.pressable} onPress={onIncrement}>
+            <Pressable style={styles.pressable} onPress={onIncrement} testID='plus-button-test-id'>
               <FontAwesome name='plus' size={12} color='darkgreen' />
             </Pressable>
           </Animated.View>
