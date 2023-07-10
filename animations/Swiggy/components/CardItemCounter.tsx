c596e2538e724a4d6b54df6ca8be40241909f17c
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-10 15:07:30 +0530

    :zap: completed card item counter component

--- animations/Swiggy/components/CardItemCounter.tsx
+++ animations/Swiggy/components/CardItemCounter.tsx
@@ -1,25 +1,58 @@
 // Packages Imports (from node_modules)
-import { StyleSheet, ViewStyle, StyleProp } from "react-native";
-import { RectButton } from "react-native-gesture-handler";
+import { StyleSheet, ViewStyle, StyleProp, View, Text, Pressable } from "react-native";
+import Animated, { FadeIn, FadeOut, ZoomIn, ZoomOut } from "react-native-reanimated";
+import { FontAwesome } from "@expo/vector-icons";
 
 // Local Imports (components/types/utils)
-import AppText from "../../../components/AppText";
+import AnimatedText from "../../../components/AnimatedText";
 
 // interface for CardItemCounter component
 export interface CardItemCounterProps {
   containerStyles?: StyleProp<ViewStyle>;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
+  currentValue?: number;
 }
 
 // functional component for CardItemCounter
 function CardItemCounter(props: CardItemCounterProps) {
   // Destructuring props
-  const { containerStyles } = props;
+  const { containerStyles, onIncrement, onDecrement, currentValue = 0 } = props;
 
   // render
   return (
-    <RectButton style={[styles.container, containerStyles]}>
-      <AppText text='ADD' color='darkgreen' style={{ fontWeight: "bold" }} />
-    </RectButton>
+    <Pressable
+      style={[styles.container, containerStyles]}
+      onPress={currentValue < 1 ? onIncrement : undefined}
+    >
+      {currentValue < 1 ? (
+        <AnimatedText
+          entering={FadeIn}
+          exiting={FadeOut.duration(50)}
+          text='ADD'
+          color='darkgreen'
+          style={{ fontWeight: "bold" }}
+        />
+      ) : (
+        <>
+          <Animated.View entering={ZoomIn} exiting={ZoomOut} style={{ flex: 1.5 }}>
+            <Pressable style={styles.pressable} onPress={onDecrement}>
+              <FontAwesome name='minus' size={12} color='darkgreen' />
+            </Pressable>
+          </Animated.View>
+
+          <Animated.View entering={FadeIn.delay(50)} style={[styles.pressable, { flex: 2 }]}>
+            <Text style={{ color: "darkgreen", fontWeight: "bold" }}>{currentValue}</Text>
+          </Animated.View>
+
+          <Animated.View entering={ZoomIn} exiting={ZoomOut} style={{ flex: 1.5 }}>
+            <Pressable style={styles.pressable} onPress={onIncrement}>
+              <FontAwesome name='plus' size={12} color='darkgreen' />
+            </Pressable>
+          </Animated.View>
+        </>
+      )}
+    </Pressable>
   );
 }
 
@@ -31,13 +64,20 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: "white",
     borderRadius: 5,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingTop: 5,
-    paddingBottom: 5,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
     elevation: 10,
     justifyContent: "center",
     alignItems: "center",
     width: 100,
+    minHeight: 30,
+    flexDirection: "row",
+  },
+  pressable: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
