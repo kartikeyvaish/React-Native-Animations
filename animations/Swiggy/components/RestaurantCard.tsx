dc881d0860c13e7192d226449e903805e32fec51
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-10 15:08:00 +0530

    :zap: added increment/decrement options

--- animations/Swiggy/components/RestaurantCard.tsx
+++ animations/Swiggy/components/RestaurantCard.tsx
@@ -1,19 +1,73 @@
 // Packages Imports (from node_modules)
+import { useState } from "react";
 import { View, StyleSheet, Text, FlatList } from "react-native";
+import Animated, { FadeIn } from "react-native-reanimated";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { RectButton } from "react-native-gesture-handler";
 
 // Local Imports (components/types/utils)
 import FoodItem from "./FoodItem";
 import Seperator from "./Seperator";
 
 // Named imports
-import { RestaurantCardProps } from "../types/Types";
+import { Cart, RestaurantCardProps } from "../types/Types";
 import { ScreenWidth } from "../../../constants/Dimensions";
 
 // functional component for RestaurantCard
 function RestaurantCard(props: RestaurantCardProps) {
   // Destructuring props
   const { items, isTrending } = props;
+
+  const [cartItems, setCartItems] = useState<Cart>({ totalItems: 0, totalPrice: 0, cartItems: {} });
+
+  const addItemToCart = (itemIndex: number) => {
+    const item = items[itemIndex];
+
+    if (item) {
+      const updatedCartItems = { ...cartItems.cartItems };
+      const updatedTotalItems = cartItems.totalItems + 1;
+      const updatedTotalPrice = cartItems.totalPrice + item.price;
+
+      let itemKey = itemIndex.toString();
+
+      if (updatedCartItems[itemKey]) {
+        updatedCartItems[itemKey] += 1;
+      } else {
+        updatedCartItems[itemKey] = 1;
+      }
+
+      setCartItems({
+        totalItems: updatedTotalItems,
+        totalPrice: updatedTotalPrice,
+        cartItems: updatedCartItems,
+      });
+    }
+  };
+
+  const removeItemFromCart = (itemIndex: number) => {
+    const item = items[itemIndex];
+
+    if (item) {
+      const updatedCartItems = { ...cartItems.cartItems };
+
+      const updatedTotalItems = cartItems.totalItems - 1;
+      const updatedTotalPrice = cartItems.totalPrice - item.price;
+
+      let itemKey = itemIndex.toString();
+
+      if (updatedCartItems[itemKey]) {
+        updatedCartItems[itemKey] -= 1;
+      } else {
+        updatedCartItems[itemKey] = 0;
+      }
+
+      setCartItems({
+        totalItems: updatedTotalItems,
+        totalPrice: updatedTotalPrice,
+        cartItems: updatedCartItems,
+      });
+    }
+  };
 
   // render
   return (
@@ -36,9 +90,49 @@ function RestaurantCard(props: RestaurantCardProps) {
             ) : null
           }
           keyExtractor={(_, index) => index.toString()}
-          renderItem={({ item }) => <FoodItem {...item} />}
+          renderItem={({ item, index }) => (
+            <FoodItem
+              {...item}
+              onIncrement={() => addItemToCart(index)}
+              onDecrement={() => removeItemFromCart(index)}
+              currentValue={cartItems.cartItems[index.toString()] || 0}
+            />
+          )}
           ItemSeparatorComponent={() => <Seperator />}
+          showsVerticalScrollIndicator={false}
         />
+
+        {cartItems.totalItems > 0 ? (
+          <Animated.View style={styles.bottomContainer} entering={FadeIn} exiting={FadeIn}>
+            <View style={{ flex: 1.5, flexDirection: "row" }}>
+              <View style={styles.restaurantLogoContainer}></View>
+
+              <Text style={styles.hotelName}>Rominus Pizza..</Text>
+            </View>
+
+            <RectButton
+              style={{
+                flex: 1,
+                backgroundColor: "#60B246",
+                borderRadius: 10,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <View style={{ flexDirection: "row", justifyContent: "center" }}>
+                <Text style={{ color: "white" }}>
+                  {cartItems.totalItems} {cartItems.totalItems > 1 ? "items" : "item"}
+                </Text>
+                <Text style={{ marginLeft: 5, marginRight: 5, color: "white" }}>|</Text>
+                <Text style={{ color: "white" }}>$ {cartItems.totalPrice}</Text>
+              </View>
+
+              <Text style={{ color: "white", textAlign: "center", fontWeight: "bold" }}>
+                Checkout
+              </Text>
+            </RectButton>
+          </Animated.View>
+        ) : null}
       </View>
     </View>
   );
@@ -77,5 +171,32 @@ const styles = StyleSheet.create({
   lightningIcon: {
     marginRight: 5,
     marginTop: 2,
+  },
+  bottomContainer: {
+    width: "100%",
+    flexDirection: "row",
+    padding: 15,
+    paddingBottom: 20,
+    paddingLeft: 10,
+    elevation: 10,
+    shadowColor: "white",
+    backgroundColor: "white",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+  },
+  restaurantLogoContainer: {
+    backgroundColor: "blue",
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  hotelName: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginLeft: 10,
   },
 });
