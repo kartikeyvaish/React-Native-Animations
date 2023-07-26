602bb08df051c5ba1e4f785279c8c4a503101847
Kartikey Vaish <kartikeyvaish99@gmail.com>
2023-07-26 21:42:34 +0530

    :zap: enhanced menu card

--- components/MenuCard.tsx
+++ components/MenuCard.tsx
@@ -1,5 +1,7 @@
 // Packages Imports
 import { StyleSheet, StyleProp, ViewStyle } from "react-native";
+import Animated, { SlideInLeft, SlideInRight } from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
 import { RectButton } from "react-native-gesture-handler";
 import { useTheme } from "@react-navigation/native";
 
@@ -9,30 +11,60 @@ import BorderView from "./BorderView";
 
 // Named imports
 import { MenuCardProps } from "../types/ComponentTypes";
+import { ScreenWidth } from "../constants/Dimensions";
 
 // function component for MenuCard
 function MenuCard(props: MenuCardProps) {
   // Destructuring props
-  const { title, onPress } = props;
+  const { title, onPress, index, colors: linearGradientColors, description = "" } = props;
 
   // Get the theme
   const { colors } = useTheme();
 
   // final styles for the card
   const finalStyles: StyleProp<ViewStyle> = [
-    styles.container,
     {
       backgroundColor: colors.background,
     },
+    styles.borderViewContainer,
   ];
 
   // render
   return (
-    <BorderView style={finalStyles}>
-      <RectButton style={{ padding: 20 }} onPress={onPress} rippleColor={colors.border}>
-        <AppText text={title} size={20} />
-      </RectButton>
-    </BorderView>
+    <Animated.View
+      style={styles.container}
+      entering={
+        index % 2 === 0
+          ? SlideInLeft.springify().delay(Math.min(index * 100, 1000))
+          : SlideInRight.springify().delay(Math.min(index * 100, 1000))
+      }
+    >
+      <BorderView style={finalStyles} borderWidth={0}>
+        <LinearGradient colors={linearGradientColors} style={styles.linearGradientContainer}>
+          <>
+            <AppText
+              style={{ fontWeight: "bold" }}
+              text={title}
+              size={18}
+              adjustsFontSizeToFit
+              numberOfLines={2}
+              color='white'
+            />
+
+            <AppText
+              text={description}
+              size={14}
+              adjustsFontSizeToFit
+              numberOfLines={8}
+              marginTop={10}
+              color='white'
+            />
+          </>
+        </LinearGradient>
+
+        <RectButton style={styles.rippleContainer} onPress={onPress} rippleColor={colors.border} />
+      </BorderView>
+    </Animated.View>
   );
 }
 
@@ -42,8 +74,27 @@ export default MenuCard;
 // styles
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 100,
+    borderRadius: 8,
+    width: ScreenWidth / 2,
+    height: ScreenWidth / 2,
+    padding: 10,
+  },
+  linearGradientContainer: {
+    ...StyleSheet.absoluteFillObject,
+    borderRadius: 8,
     overflow: "hidden",
-    marginBottom: 20,
+    padding: 10,
+  },
+  rippleContainer: {
+    ...StyleSheet.absoluteFillObject,
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  borderViewContainer: {
+    borderRadius: 8,
+    width: "100%",
+    height: "100%",
+    paddingTop: 10,
+    paddingLeft: 10,
   },
 });
